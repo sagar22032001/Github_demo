2ba9635535b1e3a708f1f8c92490cf25a8890c33
sk <kudechasadar22032001@gmail.com>
2022-10-28 16:15:42 +0530

    initial commit

---
+++ app/src/main/java/com/example/githubdemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.githubdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //before commit
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
